5e40e51a763435fea99e24019feac6256f4c4b7f
EugeneKhomitskiy <mtl_87@list.ru>
2021-09-16 22:01:25 +0300

    «Сетевые запросы: Main Thread & Background» ДЗ

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -10,7 +10,7 @@ import ru.netology.nmedia.dto.Post
 import java.util.concurrent.TimeUnit
 
 
-class PostRepositoryImpl: PostRepository {
+class PostRepositoryImpl : PostRepository {
     private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .build()
@@ -18,7 +18,7 @@ class PostRepositoryImpl: PostRepository {
     private val typeToken = object : TypeToken<List<Post>>() {}
 
     companion object {
-        private const val BASE_URL = "http://192.168.1.63:9999"
+        private const val BASE_URL = "http://192.168.1.35:9999"
         private val jsonType = "application/json".toMediaType()
     }
 
@@ -36,7 +36,24 @@ class PostRepositoryImpl: PostRepository {
     }
 
     override fun likeById(id: Long) {
+        val request: Request = Request.Builder()
+            .post(gson.toJson(id).toRequestBody(jsonType))
+            .url("${BASE_URL}/api/posts/$id/likes")
+            .build()
 
+        client.newCall(request)
+            .execute()
+            .close()
+    }
+    override fun unlikeById(id: Long) {
+        val request: Request = Request.Builder()
+            .delete(gson.toJson(id).toRequestBody(jsonType))
+            .url("${BASE_URL}/api/posts/$id/likes")
+            .build()
+
+        client.newCall(request)
+            .execute()
+            .close()
     }
 
     override fun save(post: Post) {
@@ -53,7 +70,7 @@ class PostRepositoryImpl: PostRepository {
     override fun removeById(id: Long) {
         val request: Request = Request.Builder()
             .delete()
-            .url("${BASE_URL}/api/slow/posts/$id")
+            .url("${BASE_URL}/api/posts/$id")
             .build()
 
         client.newCall(request)
