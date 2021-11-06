4c898f86baf4c625c13847e1458c26bfd1639faf
EugeneKhomitskiy <mtl_87@list.ru>
2021-11-06 21:39:59 +0300

    Black color AppBar in fragment

--- app/src/main/java/ru/netology/nmedia/activity/ImageFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/ImageFragment.kt
@@ -1,21 +1,19 @@
 package ru.netology.nmedia.activity
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import ru.netology.nmedia.BuildConfig.BASE_URL
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment() {
-
-    override fun onStart() {
-        super.onStart()
-        (activity as AppCompatActivity).supportActionBar!!.hide()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +25,13 @@ class ImageFragment : Fragment() {
             container,
             false
         )
+        // Меняем цвет панелей на черный
+        val activity = requireActivity()
+        if (activity is AppCompatActivity) {
+            val statusBarColor = ContextCompat.getColor(requireActivity(), R.color.black)
+            activity.window.statusBarColor = statusBarColor
+            activity.supportActionBar?.setBackgroundDrawable(ColorDrawable(statusBarColor))
+        }
 
         val url = "${BASE_URL}/media/${arguments?.getString("url")}"
 
@@ -35,5 +40,16 @@ class ImageFragment : Fragment() {
             .into(binding.imageView)
 
         return binding.root
+    }
+    // При закрытии фрагмента восстанавливаем исходные цвета
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val activity = requireActivity()
+        if (activity is AppCompatActivity) {
+            val statusBarColor = ContextCompat.getColor(requireActivity(), R.color.design_default_color_primary_variant)
+            val actionBarColor = ContextCompat.getColor(requireActivity(), R.color.design_default_color_primary)
+            activity.window.statusBarColor = statusBarColor
+            activity.supportActionBar?.setBackgroundDrawable(ColorDrawable(actionBarColor))
+        }
     }
 }
