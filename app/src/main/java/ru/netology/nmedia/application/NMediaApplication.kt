602738c8638a61ec661acc9acf7c5ed3bbca7243
EugeneKhomitskiy <mtl_87@list.ru>
2021-12-12 00:13:26 +0300

    changes

--- app/src/main/java/ru/netology/nmedia/application/NMediaApplication.kt
+++ app/src/main/java/ru/netology/nmedia/application/NMediaApplication.kt
@@ -1,7 +1,6 @@
 package ru.netology.nmedia.application
 
 import android.app.Application
-import androidx.hilt.work.HiltWorker
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.*
 import dagger.hilt.android.HiltAndroidApp
@@ -9,8 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.netology.nmedia.auth.AppAuth
-import ru.netology.nmedia.work.RefreshPostsWorker
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -21,36 +18,16 @@ class NMediaApplication : Application(), Configuration.Provider {
     lateinit var auth: AppAuth
 
     @Inject
-    lateinit var workManager: WorkManager
-
-    @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         setupAuth()
-        setupWork()
     }
 
     private fun setupAuth() {
         appScope.launch {
             auth.sendPushToken()
-        }
-    }
-
-    private fun setupWork() {
-        appScope.launch {
-            val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build()
-            val request = PeriodicWorkRequestBuilder<RefreshPostsWorker>(1, TimeUnit.MINUTES)
-                .setConstraints(constraints)
-                .build()
-            workManager.enqueueUniquePeriodicWork(
-                RefreshPostsWorker.name,
-                ExistingPeriodicWorkPolicy.KEEP,
-                request
-            )
         }
     }
 
